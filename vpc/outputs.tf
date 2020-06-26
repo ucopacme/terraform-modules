30175c5259bf4e04c292624d0d2cdc7249c4dc2f
Eric Odell <eric.odell@ucop.edu>
2020-06-26 15:50:38 -0700

    no nacl output

--- vpc/outputs.tf
+++ vpc/outputs.tf
@@ -7,8 +7,3 @@ output "vpc_id" {
 output "vpc_cidr_block" {
   value = join("", aws_vpc.this.*.cidr_block)
 }
-
-# VPC NACL
-output "vpc_network_acl" {
-  value = join("", aws_vpc.this.*.default_network_acl_id)
-}

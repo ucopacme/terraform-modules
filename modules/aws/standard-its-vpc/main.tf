d264b35e668e52f2e916e0668559b3e2b1c70806
Eric Odell <eric.odell@ucop.edu>
2020-07-06 21:24:20 -0700

    relative paths for elemental modules in pattern

--- modules/aws/standard-its-vpc/main.tf
+++ modules/aws/standard-its-vpc/main.tf
@@ -3,8 +3,8 @@ module "vpc" {
   cidr_block = var.cidr_block
   enabled    = var.enabled
   name       = join("-", [var.name, "vpc"])
-  #source = "git::ssh://git@github.com/ucopacme/terraform-module.git//modules/aws/vpc?ref=v0.0.1"
-  source = "../vpc?ref=v0.0.1"
+  #source = "git::ssh://git@github.com/ucopacme/terraform-module.git//modules/aws/vpc"
+  source = "../vpc"
   #source = "git::ssh://example.com/vpc.git?ref=v1.2.0"
   tags       = merge(var.tags, map("Name", var.name))
 }
@@ -15,7 +15,7 @@ module "vpc_public_subnets" {
   enabled            = var.enabled
   name               = join("-", [var.name, "vpc-public-subnet"])
   new_bits           = "2"
-  source = "../vpc_subnets?ref=v0.0.1"
+  source = "../vpc_subnets"
   subnet_cidr        = cidrsubnet(var.cidr_block, var.subnet_tier_bits, var.public_subnet_index)
   tags               = merge(var.tags, map("Name", var.name))
   vpc_id             = module.vpc.vpc_id
@@ -27,7 +27,7 @@ module "vpc_tgw_subnets" {
   enabled            = var.enabled
   name               = join("-", [var.name, "vpc-tgw-subnet"])
   new_bits           = "2"
-  source = "../vpc_subnets?ref=v0.0.1"
+  source = "../vpc_subnets"
   subnet_cidr        = cidrsubnet(var.cidr_block, var.subnet_tier_bits, var.tgw_subnet_index)
   tags               = merge(var.tags, map("Name", var.name))
   vpc_id             = module.vpc.vpc_id
@@ -39,7 +39,7 @@ module "vpc_private_subnets" {
   enabled            = var.enabled
   name               = join("-", [var.name, "vpc-private-subnet"])
   new_bits           = "2"
-  source = "../vpc_subnets?ref=v0.0.1"
+  source = "../vpc_subnets"
   subnet_cidr        = cidrsubnet(var.cidr_block, var.subnet_tier_bits, var.private_subnet_index)
   tags               = merge(var.tags, map("Name", var.name))
   vpc_id             = module.vpc.vpc_id
@@ -52,7 +52,7 @@ module "vpc_data_subnets" {
   enabled            = var.enabled
   name               = join("-", [var.name, "vpc-data-subnet"])
   new_bits           = "2"
-  source = "../vpc_subnets?ref=v0.0.1"
+  source = "../vpc_subnets"
   subnet_cidr        = cidrsubnet(var.cidr_block, var.subnet_tier_bits, var.data_subnet_index)
   tags               = merge(var.tags, map("Name", var.name))
   vpc_id             = module.vpc.vpc_id
@@ -62,7 +62,7 @@ module "vpc_data_subnets" {
 module "vpc_route_table" {
   enabled = var.enabled
   name    = join("-", [var.name, "vpc-route-table"])
-  source = "../route_table?ref=v0.0.1"
+  source = "../route_table"
   tags    = merge(var.tags, map("Name", var.name))
   vpc_id  = module.vpc.vpc_id
 }
@@ -71,7 +71,7 @@ module "vpc_route_table" {
 module "vpc_route_table_main_route_table_association" {
   enabled        = var.enabled
   name           = join("-", [var.name, "vpc-route-table-main-route-table-association"])
-  source = "../main_route_table_association?ref=v0.0.1"
+  source = "../main_route_table_association"
   tags           = merge(var.tags, map("Name", var.name))
   route_table_id = module.vpc_route_table.id
   vpc_id         = module.vpc.vpc_id
@@ -81,7 +81,7 @@ module "vpc_route_table_main_route_table_association" {
 module "vpc_igw" {
   enabled = var.enabled
   name    = join("-", [var.name, "vpc-igw"])
-  source = "../internet_gateway?ref=v0.0.1"
+  source = "../internet_gateway"
   tags    = merge(var.tags, map("Name", var.name))
   vpc_id  = module.vpc.vpc_id
 }
@@ -90,7 +90,7 @@ module "vpc_igw" {
 module "vpc_route_for_igw" {
   enabled                = var.enabled
   name                   = join("-", [var.name, "vpc-route-for-igw"])
-  source = "../route?ref=v0.0.1"
+  source = "../route"
   tags                   = merge(var.tags, map("Name", var.name))
   destination_cidr_block = "0.0.0.0/0"
   gateway_id             = module.vpc_igw.id

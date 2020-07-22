e2002b87436b342c5bac7b31d40fa9b27cbc13de
Eric Odell <eric.odell@ucop.edu>
2020-07-22 09:43:42 -0700

    arn not id derp

--- modules/aws/tgw/main.tf
+++ modules/aws/tgw/main.tf
@@ -24,11 +24,11 @@ resource "aws_ram_resource_share" "this" {
 resource "aws_ram_resource_association" "this" {
   count              = local.ram_enabled ? 1 : 0
   resource_arn       = aws_ec2_transit_gateway.this[0].arn
-  resource_share_arn = aws_ec2_transit_gateway.this[0].id
+  resource_share_arn = aws_ec2_transit_gateway.this[0].arn
 }
 
 resource "aws_ram_principal_association" "this" {
   count              = local.enabled ? length(var.ram_principals) : 0
   principal          = var.ram_principals[count.index]
-  resource_share_arn = aws_ec2_transit_gateway.this[0].id
+  resource_share_arn = aws_ec2_transit_gateway.this[0].arn
 }

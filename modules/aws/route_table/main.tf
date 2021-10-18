c850c5cae440b69bfb739fd30f58f2e8cb34ae60
Khalid Ahmadzai <khalid.ahmadzai@ucop.edu>
2021-10-18 13:10:39 -0700

    updated route table association

--- modules/aws/route_table/main.tf
+++ modules/aws/route_table/main.tf
@@ -11,7 +11,7 @@ resource "aws_route_table" "this" {
 #kk changes
 resource "aws_route_table" "private" {
   count = local.enabled ? 1 : 0
-  tags  = merge(var.tags, map("Name", var.name))
+  tags  = join("-",merge(var.tags, map("Name", var.name))"Private_route")
 
   vpc_id = var.vpc_id
 }

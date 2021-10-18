34477b0ede9d21eefd6dc23650bca00b0de0c080
Khalid Ahmadzai <khalid.ahmadzai@ucop.edu>
2021-10-18 10:57:52 -0700

    updated the route association

--- modules/aws/main_route_table_association/main.tf
+++ modules/aws/main_route_table_association/main.tf
@@ -7,3 +7,17 @@ resource "aws_main_route_table_association" "this" {
   route_table_id = var.route_table_id
   vpc_id         = var.vpc_id
 }
+
+
+resource "aws_route_table_association" "public" {
+  count = local.public_count
+
+  subnet_id      = element(modlue.aws_subnet.public.*.id, count.index)
+  route_table_id = element(aws_route_table.this.*.id, count.index)
+
+
+  depends_on = [
+    aws_subnet.public,
+    aws_route_table.public,
+  ]
+}

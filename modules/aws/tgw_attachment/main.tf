452a207fc452cd86c8bc8510601eab5066c7ede9
Khalid Ahmadzai <khalid.ahmadzai@ucop.edu>
2023-01-10 10:38:20 -0800

    updated the map func tomap

--- modules/aws/tgw_attachment/main.tf
+++ modules/aws/tgw_attachment/main.tf
@@ -4,7 +4,7 @@ locals {
 resource "aws_ec2_transit_gateway_vpc_attachment" "this" {
   count                                           = local.enabled ? 1 : 0
   subnet_ids                                      = var.subnet_ids
-  tags                                            = merge(var.tags, map("Name", var.name))
+  tags                                            = merge(var.tags, tomap({"Name" = var.name}))
   transit_gateway_default_route_table_association = var.transit_gateway_default_route_table_association
   transit_gateway_default_route_table_propagation = var.transit_gateway_default_route_table_propagation
   transit_gateway_id                              = var.transit_gateway_id
